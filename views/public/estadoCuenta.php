56773cad4f32b18c6636598807b43c1cd7bc5b74
Crack-Kstro <DarkCastroll@gmail.com>
2021-10-05 17:17:43 -0600

    Estado de cuentas control de tabla

--- views/public/estadoCuenta.php
+++ views/public/estadoCuenta.php
@@ -30,6 +30,97 @@ Public_Page::headerTemplate('SigmaQ - Estado de cuenta');
 		</div>
 	</div><br>
 </div>
+<!-- Modal  -->
+<div class="modal fade" id="modal-form" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 id="modal-title" name="modal-title" class="modal-title">Personalización de la tabla</h5>
+			</div>
+			<div class="modal-body">
+				<form method="post" id="save-form" enctype="multipart/form-data">
+					<div class="row">
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="responsable" checked>
+								<label class="form-check-label" for="responsable">Responsable</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="sociedad" checked>
+								<label class="form-check-label" for="sociedad">Sociedad</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="usuario" checked>
+								<label class="form-check-label" for="usuario">Usuario</label>
+							</div>		
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="codigo" checked>
+								<label class="form-check-label" for="codigo">Código</label>
+							</div>		
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="factura" checked>
+								<label class="form-check-label" for="factura">Factura</label>
+							</div>	
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="asignacion" checked>
+								<label class="form-check-label" for="asignacion">Asignacion</label>
+							</div>	
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="fechacontable" checked>
+								<label class="form-check-label" for="fechacontable">Fecha contable</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="clase" checked>
+								<label class="form-check-label" for="clase">Clase</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="vencimiento" checked>
+								<label class="form-check-label" for="vencimiento">Vencimiento</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="diasrestantes" checked>
+								<label class="form-check-label" for="diasrestantes">Días restantes</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="divisa" checked>
+								<label class="form-check-label" for="divisa">Divisa</label>
+							</div>
+						</div>
+						<div class="col-6 form-group">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="totalgeneral" checked>
+								<label class="form-check-label" for="totalgeneral">Total general</label>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button onclick="readRows('../../app/api/public/estadoCuenta.php?action=')" type="button" class="btn btn-primary">Guardar</button>
+			</div>
+		</div>
+	</div>
+</div>
 <!-- Seccion de tabla de registros -->
 <div class='container-fluid espacioSuperior'>
     <div class="table-responsive">
